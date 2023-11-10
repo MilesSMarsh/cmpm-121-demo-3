55a8bb626f031067ad4eed5eb60776949a8d5c30
Miles Marsh <msmarsh@ucsc.edu>
2023-11-09 17:53:11 -0800

    finished step B

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "leaflet/dist/leaflet.css";
 import "./style.css";
-import leaflet from "leaflet";
+import leaflet, { TileLayer } from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
 import { Board } from "./board.ts";
@@ -17,12 +17,14 @@ const NULL_ISLAND = leaflet.latLng({
 
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
-const PIT_SPAWN_PROBABILITY = 0.1;
+const CACHE_SPAWN_PROBABILITY = 0.1;
 const VISIBILITY_RADIUS = 8;
 
 const myBoard = new Board(TILE_DEGREES, VISIBILITY_RADIUS);
 console.log(myBoard);
+
+const arrayOfVisibleCaches: leaflet.Layer[] = [];
+const arrayOfMementos: string[] = [];
 
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
@@ -54,63 +56,114 @@ sensorButton.addEventListener("click", () => {
     playerMarker.setLatLng(
       leaflet.latLng(position.coords.latitude, position.coords.longitude)
     );
-    map.setView(playerMarker.getLatLng());
+    updatePlayer(playerMarker.getLatLng());
   });
 });
 
-let points = 0;
+const northButton = document.querySelector("#north")!;
+northButton.addEventListener("click", () => {
+  playerMarker.setLatLng(
+    leaflet.latLng(
+      playerMarker.getLatLng().lat + 1 * TILE_DEGREES,
+      playerMarker.getLatLng().lng
+    )
+  );
+  updatePlayer(playerMarker.getLatLng());
+});
+
+const southButton = document.querySelector("#south")!;
+southButton.addEventListener("click", () => {
+  playerMarker.setLatLng(
+    leaflet.latLng(
+      playerMarker.getLatLng().lat - 1 * TILE_DEGREES,
+      playerMarker.getLatLng().lng
+    )
+  );
+  updatePlayer(playerMarker.getLatLng());
+});
+
+const eastButton = document.querySelector("#east")!;
+eastButton.addEventListener("click", () => {
+  playerMarker.setLatLng(
+    leaflet.latLng(
+      playerMarker.getLatLng().lat,
+      playerMarker.getLatLng().lng + 1 * TILE_DEGREES
+    )
+  );
+  updatePlayer(playerMarker.getLatLng());
+});
+
+const westButton = document.querySelector("#west")!;
+westButton.addEventListener("click", () => {
+  playerMarker.setLatLng(
+    leaflet.latLng(
+      playerMarker.getLatLng().lat,
+      playerMarker.getLatLng().lng - 1 * TILE_DEGREES
+    )
+  );
+  updatePlayer(playerMarker.getLatLng());
+});
+
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
 
-function makePit(i: number, j: number) {
-  const bounds = leaflet.latLngBounds([
-    [
-      playerMarker.getLatLng().lat + i * TILE_DEGREES,
-      playerMarker.getLatLng().lng + j * TILE_DEGREES,
-    ],
-    [
-      playerMarker.getLatLng().lat + (i + 1) * TILE_DEGREES,
-      playerMarker.getLatLng().lng + (j + 1) * TILE_DEGREES,
-    ],
-  ]);
+function makeCache(i: number, j: number) {
+  const cell = myBoard.getCellForPoint(
+    leaflet.latLng({
+      lat: i,
+      lng: j,
+    })
+  );
+  const bounds = myBoard.getCellBounds(cell);
 
-  const pit = leaflet.rectangle(bounds) as leaflet.Layer;
+  const cache = leaflet.rectangle(bounds) as leaflet.Layer;
 
-  pit.bindPopup(() => {
-    let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+  cache.bindPopup(() => {
+    const value = Math.floor(luck([i, j, "initialValue"].toString()) * 10);
+
     const container = document.createElement("div");
     container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-                <button id="poke">Aquire</button><button id="deposit">Deposit</button>`;
-    const poke = container.querySelector<HTMLButtonElement>("#poke")!;
-    poke.addEventListener("click", () => {
-      if (value > 0) {
-        value--;
-        points++;
+                <div>There is a cache here at "${i},${j}". Collect Geocoins here.</div>`;
+    for (let x = 0; x <= value; x++) {
+      const coin = JSON.stringify({ i, j, x });
+      if (arrayOfMementos.includes(coin)) {
+        continue;
       }
-      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-        value.toString();
-      statusPanel.innerHTML = `${points} points accumulated`;
-    });
-    const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
-    deposit.addEventListener("click", () => {
-      if (points > 0) {
-        points--;
-        value++;
-      }
-      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-        value.toString();
-      statusPanel.innerHTML = `${points} points accumulated`;
-    });
+      const currCoin = document.createElement("button") as HTMLElement;
+      container.append(currCoin);
+      currCoin.innerHTML = `
+                <div>"GeoCoin: <span id="coin">${coin}</span></div>`;
+      currCoin.addEventListener("click", () => {
+        console.log(arrayOfMementos);
+        arrayOfMementos.push(coin);
+        currCoin.remove();
+      });
+    }
+
     return container;
   });
-  pit.addTo(map);
+  cache.addTo(map);
+  arrayOfVisibleCaches.push(cache);
 }
 
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-      makePit(i, j);
+function getCachesNearby(point: leaflet.LatLng) {
+  for (let i = -VISIBILITY_RADIUS; i < VISIBILITY_RADIUS; i++) {
+    for (let j = -VISIBILITY_RADIUS; j < VISIBILITY_RADIUS; j++) {
+      const x = point.lat + i * TILE_DEGREES;
+      const y = point.lng + j * TILE_DEGREES;
+      const cell = myBoard.getCellForPoint(leaflet.latLng({ lat: x, lng: y }));
+      if (luck([cell.i, cell.j].toString()) < CACHE_SPAWN_PROBABILITY) {
+        makeCache(cell.i, cell.j);
+      }
     }
   }
+}
+getCachesNearby(playerMarker.getLatLng());
+
+function updatePlayer(player: leaflet.LatLng) {
+  map.setView(player);
+  arrayOfVisibleCaches.forEach((cache) => {
+    cache.remove();
+  });
+  getCachesNearby(player);
 }
